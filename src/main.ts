610d4537534eae1af1474dee71b56deec1bb9d5b
ashhar <ashharulhaque@gmail.com>
2025-06-08 17:40:58 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  mongoose.connection.on('connected', () => {
+    console.log('connected to MongoDB');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error('Mongodb error:', err);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.warn('[MongoDB] ⚠️ Disconnected from MongoDB');
+  });
+
   await app.listen(3000);
+  console.log('application is running on port 3000');
 }
 bootstrap();
