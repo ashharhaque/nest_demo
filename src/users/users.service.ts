8fd76854193ccf0f9fecd8a4e58a7c4485bba0d8
ashhar <ashharulhaque@gmail.com>
2025-06-08 13:43:31 +0530

    done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './entity/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(userDto: any) {
+    return this.userModel.create(userDto);
+  }
+
+  findByUsername(username: string) {
+    return this.userModel.findOne({ username });
+  }
+
+  assignRole({ username, role }: { username: string; role: string }) {
+    return this.userModel.findOneAndUpdate({ username }, { role }, { new: true });
+  }
+}
